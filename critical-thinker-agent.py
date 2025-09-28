d8e6feb2e3b768a5ba974ca1268a135272b2e62e
Rishabh Bhattad <rishabh.bhattad@gmail.com>
2025-09-27 17:59:32 -0700

    Made it more functional for future

--- critical-thinker-agent.py
+++ critical-thinker-agent.py
@@ -6,26 +6,37 @@ from langchain_core.prompts import ChatPromptTemplate
 # Load env variables for os to see
 load_dotenv()
 
-gemini_api_key = os.getenv("GOOGLE_API_KEY")
-
-print(gemini_api_key)
 
 # Create llm object - automatically loads the GOGGLE_API_KEY from .env file
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-pro"
 )
 
+# Set the template for the Critical thinker
 template = ChatPromptTemplate.from_messages(
     [
-        ("system", "You are an expert critical thinker who understands details very well. You can identify conflicting statements/information in given text"),
+        ("system", "You are an expert critical thinker who understands details very well. You can identify conflicting statements/information in given text. First tell if the sentence is 'Conflicting' or 'Not Conflicting' and then provide the area where it is conflicting if it is and give reason in less than 1 line."),
         ("human", "Analyze this text: '{text_to_analyze}'")
     ]
 )
 
+# Chaining template to llm
 chain = template | llm
 
-response = chain.invoke(
-    {"text_to_analyze": "It rained the whole day today. Since I was getting bored, I got my keys to the car and went to see my friend while listening to the music on this sunny day."}
-)
+# Invoking llm from user nput
+def run_agentic_analysis(user_text: str):
+    return chain.invoke(
+        {"text_to_analyze": user_text}
+    )
 
-print(response.content)
+# Main block
+if __name__ == "__main__":
+    print("Welcome to the critical Thinker Agent! Type 'exit' instead of prompt to quit.")
+
+    # Hard coding it to run only 3 loops before it stops to not utilize resources
+    for i in range(3):
+        user_input = input("Please enter the text you want to analyze: ")
+        if user_input == 'exit':
+            break
+        response = run_agentic_analysis(user_text=user_input)
+        print(response.content)
